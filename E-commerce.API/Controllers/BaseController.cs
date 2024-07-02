62090d7b3a6d4937cd8a1b9c553e804b1c6fe038
MichaelKudron <kobrys87@gmail.com>
2024-07-02 11:09:47 +0200

    basic cotrollers added

---
+++ E-commerce.API/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using E_commerce.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_commerce.API.Controllers
+{
+    [ApiController]
+    [Route("Api/[controller]")]
+    public class BaseController:ControllerBase
+    {
+    }
+   
+}
---
+++ E-commerce.API/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+using E_commerce.Domain.Entities;
+using E_commerce.Infrastructure.Persistance;
+using Microsoft.AspNetCore.Mvc;
+namespace E_commerce.API.Controllers
+{
+    public class UserController:BaseController
+    {
+        private readonly EcommerceDbContext _context;
+
+        public UserController(EcommerceDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetUsers()
+        {
+            var users = _context.Users.ToList();
+            return users;
+        }
+        [HttpGet("id")]
+        public ActionResult<User> GetUser(int id)
+        {
+            var user = _context.Users.Find(id);
+            return user;
+        }
+    }
+}
