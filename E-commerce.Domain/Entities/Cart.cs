fd86ab8728b283dcbe593e1bf05018de8dc07c2a
Andreeeio <ninianob@gmail.com>
2024-07-03 20:25:24 +0200

    Cart

--- E-commerce.Domain/Entities/Cart.cs
+++ E-commerce.Domain/Entities/Cart.cs
@@ -1,7 +1,9 @@
 ﻿namespace E_commerce.Domain.Entities;
 public class Cart
 {
+    public Guid _Id { get; set; }
     public List<(int, Product)> _Cart { get; set; } = default!;
     public User _User { get; set; } = default!;
+    public Guid _IdUser { get; set; }
 }
 
