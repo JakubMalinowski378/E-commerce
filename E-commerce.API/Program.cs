a9e67f765c6d02a905a656b3e0ba16230ed325f1
Kuba <jakubmalinowski378@gmail.com>
2024-07-06 17:59:30 +0200

    Added authentication support in swagger

--- E-commerce.API/Program.cs
+++ E-commerce.API/Program.cs
@@ -1,11 +1,30 @@
 using E_commerce.API.Middlewares;
 using E_commerce.Application.Extensions;
 using E_commerce.Infrastructure.Extensions;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(config =>
+{
+    config.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer"
+    });
+    config.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "bearerAuth"}
+            },
+            []
+        }
+    });
+});
 builder.Services.AddInfrastucture(builder.Configuration);
 builder.Services.AddApplication(builder.Configuration);
 builder.Services.AddScoped<ErrorHandlingMiddleware>();
