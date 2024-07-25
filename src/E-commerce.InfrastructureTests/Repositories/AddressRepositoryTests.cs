331d2f8eaa50f38cb80387710c51e115773d3315
Andreeeio <ninianob@gmail.com>
2024-07-25 17:19:41 +0200

    Started doing test

---
+++ src/E-commerce.InfrastructureTests/Repositories/AddressRepositoryTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using E_commerce.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_commerce.Infrastructure.Repositories.Tests
+{
+    [TestClass()]
+    public class AddressRepositoryTests
+    {
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetByIdAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUserAddressesAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SaveChangesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ tests/E-commerce.Infrastructure.Tests/Repositories/AddressRepositoryTest.cs
@@ -0,0 +1,48 @@
+﻿using Xunit;
+using E_commerce.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_commerce.Infrastructure.Repositories.Tests;
+public class AddressRepositoryTest
+{
+
+    //Tutaj sprawdzanie poprawności bazy danych będzie
+    [Fact()]
+    public void CreateTest()
+    {
+        
+        // arrange
+
+
+        //act
+
+
+        //assert
+
+    }
+
+    [Fact()]
+    public void DeleteTest()
+    {
+        Xunit.Assert.Fail("This test needs an implementation");
+    }
+
+    [Fact()]
+    public void GetByIdAsyncTest()
+    {
+        Xunit.Assert.Fail("This test needs an implementation");
+    }
+
+    [Fact()]
+    public void GetUserAddressesAsyncTest()
+    {
+        Xunit.Assert.Fail("This test needs an implementation");
+    }
+
+    [Fact()]
+    public void SaveChangesTest()
+    {
+        Xunit.Assert.Fail("This test needs an implementation");
+    }
+}
+
