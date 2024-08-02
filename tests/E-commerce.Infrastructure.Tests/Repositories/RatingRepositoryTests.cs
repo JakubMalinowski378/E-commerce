6b236779899b29662fd549ac45b1e3e5dfba89d1
Andreeeio <ninianob@gmail.com>
2024-08-02 12:57:01 +0200

    UserRepositoryTest started

--- tests/E-commerce.Infrastructure.Tests/Repositories/RatingRepositoryTests.cs
+++ tests/E-commerce.Infrastructure.Tests/Repositories/RatingRepositoryTests.cs
@@ -130,23 +130,75 @@ public class RatingRepositoryTests
     public async Task GetRatingByUserIdAndProductId()
     {
         // arrange
-
+        User user = new User()
+        {
+            Id= Guid.NewGuid(),
+            Firstname = "Tester",
+        };
+        Product product = new Product()
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test",
+        };
+        Rating rating = new Rating()
+        {
+            Id = Guid.NewGuid(),
+            UserId = user.Id,
+            ProductId = product.Id,
+            AddedDate = DateTime.Now,
+            Rate = Domain.Constants.Ratings.VeryGood,
+            Comment = "Masterpiece",
+        };
+        Rating rating2 = new Rating()
+        {
+            Id = Guid.NewGuid(),
+            UserId = Guid.NewGuid(),
+            ProductId = product.Id,
+            AddedDate = new DateTime(2022, 3, 5),
+            Rate = Domain.Constants.Ratings.Good,
+            Comment = "Kinda good",
+        };
 
         //act
+        await _ratingRepository.CreateRating(rating);
+        await _ratingRepository.CreateRating(rating2);
+        var ratingsFromDb = await _ratingRepository.GetRatingByUserIdAndProductId(user.Id, product.Id);
 
         //assert
-
+        Assert.Equal(ratingsFromDb.AddedDate,rating.AddedDate);
     }
 
     [Fact()]
     public async Task GetRatings()
     {
         // arrange
-
+        Rating rating = new Rating()
+        {
+            Id = Guid.NewGuid(),
+            UserId = Guid.NewGuid(),
+            ProductId = Guid.NewGuid(),
+            AddedDate = DateTime.Now,
+            Rate = Domain.Constants.Ratings.VeryGood,
+            Comment = "Masterpiece",
+        };
+        Rating rating2 = new Rating()
+        {
+            Id = Guid.NewGuid(),
+            UserId = Guid.NewGuid(),
+            ProductId = Guid.NewGuid(),
+            AddedDate = new DateTime(2022, 3, 5),
+            Rate = Domain.Constants.Ratings.Good,
+            Comment = "Kinda good",
+        };
 
         //act
+        await _ratingRepository.CreateRating(rating);
+        await _ratingRepository.CreateRating(rating2);
+        var ratingsFromDb = await _ratingRepository.GetRatings();
 
         //assert
+        Assert.Contains(ratingsFromDb, r => r.AddedDate == rating.AddedDate && r.Rate == rating.Rate && r.Comment == rating.Comment);
+        Assert.Contains(ratingsFromDb, r => r.AddedDate == rating2.AddedDate && r.Rate == rating2.Rate && r.Comment == rating2.Comment);
 
     }
 
---
+++ tests/E-commerce.Infrastructure.Tests/Repositories/RolesRepositoryTest.cs
@@ -0,0 +1,43 @@
+﻿using E_commerce.Domain.Entities;
+using E_commerce.Domain.Repositories;
+using E_commerce.Infrastructure.Persistance;
+using E_commerce.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_commerce.Infrastructure.Tests.Repositories;
+
+public class RolesRepositoryTest
+{
+    private readonly DbContextOptions<EcommerceDbContext> _dbContextOptions;
+    private readonly EcommerceDbContext _context;
+    private readonly IRolesRepository _rolesRepository;
+
+    public RolesRepositoryTest()
+    {
+        _dbContextOptions = new DbContextOptionsBuilder<EcommerceDbContext>()
+                            .UseInMemoryDatabase(databaseName: "TestDatabase")
+                            .Options;
+        _context = new EcommerceDbContext(_dbContextOptions);
+        _rolesRepository = new RolesRepository(_context);
+    }
+
+    [Fact()]
+    public async Task GetRole()
+    {
+        // arrange
+        await _context.Roles.AddRangeAsync(new Role
+        {
+            Id = Guid.NewGuid(),
+            Name = "Test",
+        }
+        );
+        _context.SaveChanges();
+        
+
+        //act
+        var roleFromDb = await _rolesRepository.GetRole("Test");
+
+        //assert
+        Assert.Equal(roleFromDb.Name,"Test");
+   }
+}
